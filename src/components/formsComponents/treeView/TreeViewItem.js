d543b2bf91ae1f107fbd104deadff6b0c77727c7
AlexARtS7 <enginpro@yandex.ru>
2022-10-27 12:58:48 +0400

    base client 1.6.8

--- src/components/formsComponents/treeView/TreeViewItem.js
+++ src/components/formsComponents/treeView/TreeViewItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { ArrowRight, CaretDownSquare, CaretRightSquare } from 'react-bootstrap-icons'
+import { ArrowReturnRight, ArrowRight, CaretDownSquare, CaretRightSquare, ChevronDown, ChevronRight, JournalMinus, JournalPlus } from 'react-bootstrap-icons'
 import './treeView.scss'
 
 const TreeViewItem = (props) => {
@@ -7,15 +7,23 @@ const TreeViewItem = (props) => {
   const [visible, setVisible] = useState(false)
   const children = categories.filter(i => i.categoryId === e.id)
 
+  const onTreeClick = (elem) => {
+    elem.stopPropagation()
+    setVisible(!visible)
+  }
+
   useEffect(() => {
     if(!vis) setVisible(vis)
   }, [vis])
 
   return (
     <ul>
-      
-      <li className={vis ? 'none':'collapse'} role='button' onClick={() => setVisible(!visible)}>
-        {visible? <CaretDownSquare className='me-1'/>:<CaretRightSquare className='me-1'/>}
+      <li className={vis ? 'none':'collapse'} role='button'>
+        {categories.find(i => i.categoryId === e.id) ?
+          <span className='me-2' onClick={elem => onTreeClick(elem)}>{visible? <ChevronDown/>:<ChevronRight/>}</span>
+        
+        :
+        <ArrowReturnRight className='me-2'/>}
         {e.name}
       </li>
       {children.length > 0 && children.map((e,i) => <TreeViewItem key={i}  e={e} categories={categories} vis={visible}/>)}
--- src/components/modals/OffCanvasModal.js
+++ src/components/modals/OffCanvasModal.js
@@ -10,36 +10,7 @@ const OffCanvasModal = ({show, setShow}) => {
             <Offcanvas.Title>Выберите раздел</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body> <hr/>
-            <TreeViewList/>
-                {/* <ListGroup variant="flush" >
-                    <ListGroup.Item as='ul'>Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item as='ul'>Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                    <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                </ListGroup> */}
-                {/* <ul>
-                    <li>
-                        <div className='el'>Root</div>
-                            <ul className='ce'>
-                                <li >
-                                    <div className='cl el'>Item 1</div>
-                                </li>
-                                <li >
-                                    <div>Item 1</div>
-                                </li>
-                                    <ul >
-                                        <li >
-                                            <div>Item 1</div>
-                                        </li>
-                                        <li >
-                                            <div>Item 1</div>
-                                        </li>
-                                    </ul>
-                            </ul>
-                    </li>
-                </ul> */}
-
+                <TreeViewList/>
             </Offcanvas.Body>
         </Offcanvas>
     )
