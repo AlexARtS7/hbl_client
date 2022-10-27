08f63be4206fbbadf968461f55c6f125493ae67d
AlexARtS7 <enginpro@yandex.ru>
2022-10-27 11:02:34 +0400

    base client 1.6.7

--- src/components/formsComponents/treeView/TreeViewItem.js
+++ src/components/formsComponents/treeView/TreeViewItem.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { ArrowRight, CaretDownSquare, CaretRightSquare } from 'react-bootstrap-icons'
+import './treeView.scss'
 
-const TreeViewItem = () => {
-    const [trigger, setTrriger] = useState(false)
+const TreeViewItem = (props) => {
+  const {e, categories, vis = true} = props
+  const [visible, setVisible] = useState(false)
+  const children = categories.filter(i => i.categoryId === e.id)
+
+  useEffect(() => {
+    if(!vis) setVisible(vis)
+  }, [vis])
+
   return (
-    <div>TreeViewItem</div>
+    <ul>
+      
+      <li className={vis ? 'none':'collapse'} role='button' onClick={() => setVisible(!visible)}>
+        {visible? <CaretDownSquare className='me-1'/>:<CaretRightSquare className='me-1'/>}
+        {e.name}
+      </li>
+      {children.length > 0 && children.map((e,i) => <TreeViewItem key={i}  e={e} categories={categories} vis={visible}/>)}
+    </ul>    
   )
 }
 
